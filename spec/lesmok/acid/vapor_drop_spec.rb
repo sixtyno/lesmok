c3469c5b2c3a7b1109f8de3769290c7ca5d9bd70
Kent Dahl @ Sixty <kent@sixty.no>
2015-09-17 12:18:34 +0200

    Spec cleanup.

--- spec/lesmok/acid/vapor_drop_spec.rb
+++ spec/lesmok/acid/vapor_drop_spec.rb
@@ -3,10 +3,10 @@ describe ::Lesmok::Acid::VaporDrop do
 
   let(:source_klass) do
     Struct.new(:name, :age, :phone) do
-    	include ::Lesmok::Acid::Meltable
-    	def birth_year
-    		2015 - age
-    	end
+      include ::Lesmok::Acid::Meltable
+      def birth_year
+        2015 - age
+      end
     end
   end
   let(:source) do
@@ -14,7 +14,7 @@ describe ::Lesmok::Acid::VaporDrop do
   end
   let(:drop) do
     described_class.new(name: 'John', age: 36, phone: 'N/A') do |data|
-    	source
+      source
     end
   end
 
@@ -23,34 +23,34 @@ describe ::Lesmok::Acid::VaporDrop do
   end
 
   it "returns pre-given data instead of fetching source" do
-    expect(drop.name).to  be_eql("John")
-    expect(drop.age).to   be == 36
-    expect(drop.phone).to be_eql("N/A")
+    expect(drop.name).to  eql("John")
+    expect(drop.age).to   eql(36)
+    expect(drop.phone).to eql("N/A")
   end
 
   it "can fetch source liquid object" do
-  	expect(drop.condensed_acid_drop?).to be_falsey
-  	expect(drop.condense_acid_drop!).to  be_truthy
-  	expect(drop.condensed_acid_drop?).to be_truthy
-    expect(drop.source_object).to 			 be_truthy
+    expect(drop.condensed_acid_drop?).to be_falsey
+    expect(drop.condense_acid_drop!).to  be_truthy
+    expect(drop.condensed_acid_drop?).to be_truthy
+    expect(drop.source_object).to        be_truthy
     expect(drop.source_object.class).to be(::Lesmok::Acid::Drop)
     expect(drop.source_object.source_object).to be == source
   end
 
   it "will try to fetch source for unknown method calls" do
-  	expect(drop.condensed_acid_drop?).to be_falsey
-  	expect(drop.birth_year).to  be == 1978
-  	expect(drop.condensed_acid_drop?).to be_truthy
+    expect(drop.condensed_acid_drop?).to be_falsey
+    expect(drop.birth_year).to  eql(1978)
+    expect(drop.condensed_acid_drop?).to be_truthy
   end
 
   it "delegates after source has been fetched pre-given data instead of fetching source" do
-    expect(drop.name).to  be_eql("John")
-    expect(drop.age).to   be == 36
-    expect(drop.phone).to be_eql("N/A")
-  	expect(drop.birth_year).to  be == 1978
-    expect(drop.name).to  be_eql("John Doe")
-    expect(drop.age).to   be == 37
-    expect(drop.phone).to be_eql(source.phone)
+    expect(drop.name).to  eql("John")
+    expect(drop.age).to   eql(36)
+    expect(drop.phone).to eql("N/A")
+    expect(drop.birth_year).to eql(1978)
+    expect(drop.name).to  eql("John Doe")
+    expect(drop.age).to   eql(37)
+    expect(drop.phone).to eql(source.phone)
   end
 
 
